ffb8ec4fb985277c042423c2312a3b12f17be22f
HanifHosseini <hanifhos@gmail.com>
2021-03-29 22:31:16 +0000

    "day11111111111111111111111111"

--- head.js
+++ head.js
@@ -5,7 +5,12 @@ const assertEqual = function (actual, expected){
   else {console.log(actual + " !=== " + expected) }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+const head = function(array){
+  return array[0];
+}
 
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+console.log(assertEqual)
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+console.log(assertEqual)
 
